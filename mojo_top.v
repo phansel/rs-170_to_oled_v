1529ecd507d44ab23833177def33f05292eb46b1
phansel <paul.j.hans.el+geethoob@gmail.com>
2015-10-22 20:31:46 -0400

    Create mojo_top.v

---
+++ mojo_top.v
@@ -0,0 +1,12 @@
+module mojo_top (
+  input clk,
+  input rst,
+  input [7:0] adc_data;
+  output adc_en;
+  
+  output [7:0] oled_data,
+  output oled_e,
+  output oled_rw,
+  output oled_dc,
+  output oled_cs
+);
